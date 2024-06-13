b792e6968aa19f322c8afc5a60dbb01a6961e707
lucaNeves <luizneves6@hotmail.com>
2024-06-13 17:29:55 -0300

    finalizando o desafio

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+let nomeHeroi = "Luffy"
+let xp = 6520
+let nivelHeroi
+
+if (xp <= 1000) {
+    nivelHeroi = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivelHeroi = "Imortal";
+} else if (xp >= 10001) {
+    nivelHeroi = "Radiante";
+}
+
+let mensagemFinal = "O Herói de nome " + nomeHeroi + " está no nível de "+nivelHeroi
+console.log(mensagemFinal)
